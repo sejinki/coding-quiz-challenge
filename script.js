df10eb93a39115b40e28e002e6a66ffa24ffb1c1
Sejin <kisejin2015@gmail.com>
2022-07-09 18:48:25 -0600

    prevent default

---
+++ script.js
@@ -0,0 +1,10 @@
+var startButton = document.querySelector('#startButton')
+
+startButton.addEventListener('click', function(){
+console.log('this is where i need to start a timer');
+console.log('')
+})
+
+event.preventDefault();
+
+
