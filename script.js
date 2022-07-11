f3805dffc35023a5660e00a43f2f992540f8e902
Sejin <kisejin2015@gmail.com>
2022-07-11 14:41:25 -0600

    progress

--- script.js
+++ script.js
@@ -1,10 +1,58 @@
 var startButton = document.querySelector('#startButton')
+var timer = document.querySelector('#timer');
+var question = document.querySelector('#question');
+var choices = document.querySelector('#choices');
 
-startButton.addEventListener('click', function(){
-console.log('this is where i need to start a timer');
-console.log('')
+var time = 2;
+var index = 0;
+var questions = [
+    {
+        question: "What is 1+1?",
+        choices: [0,1,2,3],
+        answer: 2
+    },
+    {
+        question:"What is 1+2?",
+        choices: [0,1,2,3],
+        answer: 3
+    },
+    {
+        question:"What is 1-1?",
+        choices: [0,1,2,3],
+        answer: 0
+    },
+]
+
+function displayTime() {
+    timer.textContent = time;
+    var countDown = setInterval(function () {
+        time--;
+        console.log(time)
+        timer.textContent = time;
+        
+        if(time <= 0){
+            clearInterval(countDown);
+        }
+
+    }, 1000);
+
+}
+
+function displayQuestions (){
+    question.textContent = questions[index].question
+
+
+}
+
+startButton.addEventListener('click', function () {
+    console.log('this is where i need to start a timer');
+
+    displayTime();
+    displayQuestions();
+    console.log('')
 })
 
-event.preventDefault();
+// event.preventDefault();
+
 
 
