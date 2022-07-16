107fa1759ded1e64de80181109f681bbe265de7f
Sejin <kisejin2015@gmail.com>
2022-07-15 18:03:29 -0600

    score

--- script.js
+++ script.js
@@ -12,30 +12,30 @@ var index = 0;
 var questions = [
     {
         question: "What is a boolean?",
-        choices: ["1. true or false value", 
-        "2. it is a number", 
-        "3. it is something random", 
-        "4. it is none of the above"],
+        choices: ["1. true or false value",
+            "2. it is a number",
+            "3. it is something random",
+            "4. it is none of the above"],
         answer: 0
     },
     {
-        question:"An array is signaled by what?",
-        choices: [0,1,2,3],
+        question: "An array is signaled by what?",
+        choices: [0, 1, 2, 3],
         answer: 3
     },
     {
-        question:"A string is placed in what?",
-        choices: [0,1,2,3],
+        question: "A string is placed in what?",
+        choices: [0, 1, 2, 3],
         answer: 0
     },
     {
-        question:"The most important datatype in JS is?",
-        choices: [0,1,2,3],
+        question: "The most important datatype in JS is?",
+        choices: [0, 1, 2, 3],
         answer: 0
     },
     {
-        question:"A method is composed of?",
-        choices: [0,1,2,3],
+        question: "A method is composed of?",
+        choices: [0, 1, 2, 3],
         answer: 0
     },
     // {
@@ -57,8 +57,8 @@ function displayTime() {
         time--;
         console.log(time)
         timer.textContent = time;
-        
-        if(time <= 0){
+
+        if (time <= 0) {
             endQuiz();
             clearInterval(countDown);
         }
@@ -67,37 +67,39 @@ function displayTime() {
 
 }
 
-function displayQuestions (){
+function displayQuestions() {
     question.textContent = questions[index].question;
     var currentQuestion = questions[index];
     console.log(currentQuestion);
     choices.innerHTML = '';
-    for(var i = 0; i < currentQuestion.choices.length; i++){
+    for (var i = 0; i < currentQuestion.choices.length; i++) {
         var choicesButton = document.createElement('button');
         var choice = currentQuestion.choices[i];
         console.log(choice);
         choicesButton.textContent = choice;
         choicesButton.className = "choices"
         choices.append(choicesButton);
-        choicesButton.addEventListener('click' , nextQuestion);
+        choicesButton.addEventListener('click', nextQuestion);
     }
 
-    function nextQuestion (event) {
-    var rightAnswer = questions[index].answer;
-    rightAnswer = questions[index].choices[rightAnswer];
-    if (rightAnswer== event.target.innerHTML){
-        score++;
-        console.log(score);
+    function nextQuestion(event) {
+        var rightAnswer = questions[index].answer;
+        rightAnswer = questions[index].choices[rightAnswer];
+        if (rightAnswer == event.target.innerHTML) {
+            score++;
+            console.log(score);
+        } else if (rightAnswer != event.target.innerHTML) {
+            time -= 5;
+        }
+        index++;
+        if (index < questions.length) {
+            displayQuestions();
+        } else {
+            endQuiz()
+        }
     }
-    index++; 
-    if (index < questions.length ) {
-    displayQuestions();   
-    } else {
-        endQuiz()
-    }
-    }
-    
-    
+
+
     //display each choice on each button
     //append each button to choices div
     //create onclick event for each button
@@ -107,9 +109,9 @@ function displayQuestions (){
     // what are your initials
 
 }
-function endQuiz (){
+function endQuiz() {
     clearInterval(countDown);
-    qContainer.className ='hidden';
+    qContainer.className = 'hidden';
     results.innerHTML = 'your score is ' + score;
     var form = document.createElement('div');
     results.append(form);
@@ -117,17 +119,35 @@ function endQuiz (){
     form.innerHTML = 'type initials';
     form.append(initials);
     var inButton = document.createElement('button');
-    inButton.addEventListener('click', function(){
+    inButton.addEventListener('click', function () {
         submitScore(initials.value);
     });
     form.append(inButton);
     inButton.innerText = 'submit';
- }
+}
 
 
- function submitScore(initials){
-console.log(initials, score);
- }
+var initialArr = JSON.parse(localStorage.getItem("initialArr")) || [];
+var highscoreDiv = document.getElementById("highscore")
+function submitScore(initials) {
+    console.log(initials, score);
+    initialArr.push(
+        {
+            initial: initials,
+            score: score
+        })
+    localStorage.setItem("initialArr", JSON.stringify(initialArr))
+    displayHighscore()
+}
+
+function displayHighscore() {
+    highscoreDiv.innerHTML = "";
+    initialArr.forEach(function (data) {
+        var p = document.createElement("p");
+        p.innerHTML = data.initial + " - " + data.score;
+        highscoreDiv.appendChild(p)
+    })
+}
 
 startButton.addEventListener('click', function () {
     console.log('this is where i need to start a timer');
@@ -137,6 +157,8 @@ startButton.addEventListener('click', function () {
     displayQuestions();
     console.log('')
 })
+
+displayHighscore()
 
 // event.preventDefault();
 
